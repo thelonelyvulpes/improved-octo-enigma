7872a66eef5db866bbad666ba8d086a75473006f
Gregory Woods <gregory.woods@neo4j.com>
2024-06-27 19:34:24 +0100

    Nodes and Rels and a lil bit of Path

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::{FixedOffset, TimeDelta};
+use chrono::{FixedOffset};
 use serde::{Deserialize, Deserializer};
 use std::collections::HashMap;
 
@@ -76,6 +76,20 @@ pub enum Val {
         )]
         value: chrono::TimeDelta,
     },
+    Node {
+        #[serde(rename = "_value")]
+        value: Node,
+    },
+    Relationship {
+        #[serde(rename = "_value")]
+        value: Relationship,
+    },
+    Path {
+        #[serde(
+            rename = "_value",
+            deserialize_with = "try_path",)]
+        value: Path,
+    },
 }
 
 fn try_i64_from_str<'de, D>(deserializer: D) -> Result<i64, D::Error>
@@ -139,8 +153,93 @@ fn try_duration_from_str<'de, D>(deserializer: D) -> Result<chrono::TimeDelta, D
     where
         D: Deserializer<'de>,
 {
-    let s: String = Deserialize::deserialize(deserializer).unwrap();
+    let _: String = Deserialize::deserialize(deserializer).unwrap();
     Ok(chrono::TimeDelta::new(0,0).unwrap())
+}
+
+fn try_path<'de, D>(deserializer: D) -> Result<Path, D::Error>
+    where
+        D: Deserializer<'de>,
+{
+    // let node = deserializer.deserialize_struct(Node {})
+    Ok(Path { nodes: vec![], relationships: vec![] })
+}
+
+// struct PathVisitor;
+//
+// impl<'de> Visitor<'de> for PathVisitor {
+//     type Value = Path;
+//
+//     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+//         formatter.write_str("struct Path")
+//     }
+//
+//     fn visit_seq<V>(self, mut seq: V) -> Result<Path, V::Error>
+//     where
+//         V: SeqAccess<'de>,
+//     {
+//         let mut nodes = Vec::new();
+//         let mut relationships = Vec::new();
+//
+//
+//         while let elem = seq.next_element()? {
+//
+//             match elem {  }
+//             vec.push(elem);
+//         }
+//         while let Some(value) = seq.next_element::<Value>()? {
+//
+//             let my_val = serde_json::Deserializer::from().into_iter::<Body>();
+//
+//             match value {
+//                 Value::Null => {}
+//                 Value::Bool(_) => {}
+//                 Value::Number(_) => {}
+//                 Value::String(_) => {}
+//                 Value::Array(_) => {}
+//                 Value::Object(_) => {}
+//             }
+//             if value.get("type").is_some() {
+//                 let relationship: Relationship = serde_json::from_value(value).map_err(de::Error::custom)?;
+//                 relationships.push(relationship);
+//             } else {
+//                 let node: Node = serde_json::from_value(value).map_err(de::Error::custom)?;
+//                 nodes.push(node);
+//             }
+//         }
+//
+//         Ok(Path { nodes, relationships })
+//     }
+// }
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Node {
+    #[serde(rename = "_labels")]
+    pub labels: Vec<String>,
+    #[serde(rename = "_properties")]
+    pub properties: HashMap<String, Val>,
+    #[serde(rename = "_element_id")]
+    pub element_id: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Relationship {
+    #[serde(rename = "_type")]
+    pub type_: String,
+    #[serde(rename = "_properties")]
+    pub properties: HashMap<String, Val>,
+    #[serde(rename = "_element_id")]
+    pub element_id: String,
+    #[serde(rename = "_start_node_element_id")]
+    pub start_node_element_id: String,
+    #[serde(rename = "_end_node_element_id")]
+    pub end_node_element_id: String,
+}
+#[derive(Deserialize, Debug, Clone)]
+pub struct Path {
+    pub nodes: Vec<Node>,
+    pub relationships: Vec<Relationship>
 }
 
 //
@@ -370,3 +469,125 @@ fn nest_list_deserializes() {
         };
     }
 }
+
+#[test]
+fn node_deserializes() {
+
+    let test = "{
+                \"$type\": \"Node\",
+                \"_value\": {
+                    \"_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:13\",
+                    \"_labels\": [\"Person\"],
+                    \"_properties\": {
+                        \"name\": {
+                            \"$type\": \"String\",
+                            \"_value\": \"Richard\"
+                        }
+                    }
+                }
+            }";
+    let inputs = serde_json::Deserializer::from_str(test).into_iter::<Body>();
+    for input in inputs {
+        match *input.unwrap().body {
+            Val::Node { value } => {
+                assert_eq!("4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:13", value.element_id);
+                let labels: &[String] = &*value.labels;
+                assert_eq!(labels,value.labels);
+                assert_eq!(1,value.properties.len());
+
+                match &value.properties.get("name").unwrap() {
+                    Val::String { value } => {
+                        assert_eq!("Richard", value)
+                    }
+                    _ => panic!("test fail")
+                }
+            }
+            _ => panic!("test fail"),
+        };
+    }
+}
+
+#[test]
+fn rel_deserializes() {
+    let test = "{
+                \"$type\": \"Relationship\",
+                \"_value\": {
+                    \"_element_id\": \"5:ca452f2f-1fbe-4d91-8b67-486b237e24c5:1152921504606846989\",
+                    \"_start_node_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:13\",
+                    \"_end_node_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:14\",
+                    \"_type\": \"RIDES\",
+                    \"_properties\": {
+                        \"name\": {
+                            \"$type\": \"String\",
+                            \"_value\": \"Richard\"
+                        }
+                    }
+                }
+            }";
+    let inputs = serde_json::Deserializer::from_str(test).into_iter::<Body>();
+    for input in inputs {
+        match *input.unwrap().body {
+            Val::Relationship { value } => {
+                assert_eq!("5:ca452f2f-1fbe-4d91-8b67-486b237e24c5:1152921504606846989", value.element_id);
+                assert_eq!("4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:13", value.start_node_element_id);
+                assert_eq!("4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:14", value.end_node_element_id);
+                assert_eq!("RIDES", value.type_);
+                assert_eq!(1, value.properties.len());
+
+                match &value.properties.get("name").unwrap() {
+                    Val::String { value } => {
+                        assert_eq!("Richard", value)
+                    }
+                    _ => panic!("test fail")
+                }
+            }
+            _ => panic!("test fail"),
+        };
+    }
+}
+
+#[test]
+fn path_deserializes() {
+    let test = "{
+                \"$type\": \"Path\",
+                \"_value\": [
+                    {
+                        \"$type\": \"Node\",
+                        \"_value\": {
+                            \"_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:13\",
+                            \"_labels\": [\"Person\"],
+                            \"_properties\": {}
+                        }
+                    },
+                    {
+                        \"$type\": \"Relationship\",
+                        \"_value\": {
+                            \"_element_id\": \"5:ca452f2f-1fbe-4d91-8b67-486b237e24c5:1152921504606846989\",
+                            \"_start_node_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:13\",
+                            \"_end_node_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:14\",
+                            \"_type\": \"RIDES\",
+                            \"_properties\": {}
+                        }
+                    },
+                    {
+                        \"$type\": \"Node\",
+                        \"_value\": {
+                            \"_element_id\": \"4:ca452f2f-1fbe-4d91-8b67-486b237e24c5:14\",
+                            \"_labels\": [\"Bicycle\"],
+                            \"_properties\": {}
+                        }
+                    }
+                ]
+            }";
+    let inputs = serde_json::Deserializer::from_str(test).into_iter::<Body>();
+    for input in inputs {
+        match *input.unwrap().body {
+            Val::Path { value } => {
+                assert_eq!(0, value.nodes.len());
+                assert_eq!(0, value.relationships.len());
+            }
+            _ => panic!("test fail"),
+        };
+    }
+}
+
